c7c5e163a1c3105671a96929e121a61c997106be
giorgenes <giorgenes@gmail.com>
2021-10-16 07:19:18 +0000

    WIP

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const pug = require('pug');
 
 // Compile the source code
-const compiledFunction = pug.compileFile('template.pug');
+const compiledFunction = pug.compileFile('templates/index.pug');
 
 // Render a set of data
 console.log(compiledFunction({
